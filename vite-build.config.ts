e7aaec38d805a4a6884212b8aa48c80313b66053
ikcede <1250975+ikcede@users.noreply.github.com>
2024-07-19 18:32:46 -0400

    Fix build configs to properly build types

---
+++ vite-build.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+
+/**
+ * Config file for building main.js and main.d.ts
+ */
+export default defineConfig({
+  base: '',
+  root: '.',
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    dts({
+      exclude: [
+        '**/stories',
+        '**/test',
+      ],
+      insertTypesEntry: true,
+      rollupTypes: true,
+    })
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: 'src/components/index.ts',
+      formats: ['es'],
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'main.js',
+      }
+    }
+  },
+})
